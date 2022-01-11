6f87be980c6002a348db21819a1a0c81330e703d
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2022-01-11 19:06:34 -0300

    Iniciando protótipo de Global Community.

---
+++ src/main/java/br/com/parceria_projeto/prototype/global_community/global_community/GlobalCommunityApplication.java
@@ -0,0 +1,13 @@
+package br.com.parceria_projeto.prototype.global_community.global_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/parceria_projeto/prototype/global_community/global_community/GlobalCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.parceria_projeto.prototype.global_community.global_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
